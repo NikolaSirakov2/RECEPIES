0aa8588e03af285f6d2314e3789ecfe271a346a2
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-20 11:37:34 +0200

    Update ViewController.js

--- ViewController.js
+++ ViewController.js
@@ -1,47 +1,88 @@
 class ViewController {
-
-constructor() {
-    window.addEventListener("hashchange", this.changeMainPage);
-    window.addEventListener("load", this.changeMainPage);
+  constructor() {
+    window.addEventListener("hashchange", this.handleHashChange);
+    window.addEventListener("load", this.handleHashChange);
     this.recepiesManager = new RecipiesManager();
   }
 
-  changeMainPage = () => {
-    let hash = window.location.hash.slice(1);
+  handleHashChange = () => {
+    let hash = window.location.hash.slice(1) || "allRecepies";
 
-    let allRecepies = document.getElementById("allRecepies");
-    let favoriteRecepie = document.getElementById("favoriteRecepie");
-    let createRecepie = document.getElementById("createRecepie");
+    const pageIds = [
+      "allRecepies",
+      "favoriteRecepie",
+      "createRecepie",
+      "myProfile",
+      "errorPage",
+    ];
 
-    if (hash === "allRecepies") {
-      allRecepies.style.display = "flex";
-      favoriteRecepie.style.display = "none";
-      createRecepie.style.display = "none";
-    } else if (hash === "favoriteRecepie") {
-      allRecepies.style.display = "none";
-      favoriteRecepie.style.display = "flex";
-      createRecepie.style.display = "none";
-    } else if (hash === "createRecepie") {
-      allRecepies.style.display = "none";
-      favoriteRecepie.style.display = "none";
-      createRecepie.style.display = "flex";
-    }
+    pageIds.forEach((id) => {
+      let page = document.getElementById(id);
+
+      if (hash === id) {
+        page.style.display = "block";
+      } else {
+        page.style.display = "none";
+      }
+    });
 
     switch (hash) {
       case "allRecepies":
-        this.renderallRecepiesPage();
+        this.renderAllRecepiesPage();
         break;
       case "favoriteRecepie":
-        this.renderfavoriteRecepiePage();
-        break;
-      case "createRecepie":
-        this.renderfavoriteRecepiePage();
-        break;
+        this.renderFavoriteRecepies();
     }
   };
 
+  renderRecepies = (recipeList, container) => {
+        container.innerHTML = "";
 
+        recipeList.forEach((recipe) => {
+        let card = createElement("div");
+        card.classList.add("card");
+        card.style.width = "200px";
+  
+        let tumbnail = createElement("img");
+        tumbnail.src = recipe.thumbnail;
+        tumbnail.style.width = "200px";
+  
+        let title = createElement("div");
+        title.innerText = recipe.title;
+  
+        let ingredients = createElement("div");
+        ingredients.innerText = recipe.ingredients;
+  
+        let addToFavorites = createElement("button");
+        addToFavorites.classList = "add"
+        addToFavorites.innerText = "Добави в любими";
+  
+        let cook = createElement("button");
+        cook.innerText = "Сготви";
+  
+        card.append(tumbnail, title, ingredients, addToFavorites, cook);
+  
+        container.appendChild(card);
+      });
+  }
 
-}  
+  renderAllRecepiesPage = () => {
+    let searchByName = document.getElementById("searchByName");
+
+    searchByName.addEventListener("input", (e) => {
+        let result = this.recepiesManager.search(e.target.value);
+
+        this.renderRecepies(result, recepiesContainer);
+    });
+
+    let recepiesContainer = document.querySelector("#allRecepies .container");
+
+    this.renderRecepies(this.recepiesManager.recipeList, recepiesContainer);
+  };
+
+  renderFavoriteRecepies = () => {
+
+  };
+}
 
 let viewController = new ViewController();
