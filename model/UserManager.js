354af961a5f7891aa312ee236afcfee4183d89de
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-20 17:55:29 +0200

    Create UserManager.js

---
+++ model/UserManager.js
@@ -0,0 +1,15 @@
+class User {
+    constructor(name, age, address, photo){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.photo = photo;
+    }
+};
+
+
+class UserManager {
+
+    userInfo = [];
+
+}
