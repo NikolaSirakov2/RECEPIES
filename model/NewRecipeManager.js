9d6d02df588054ce0c55c82b9bdec2b5fb721195
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-20 13:45:00 +0200

    Create NewRecipeManager.js

---
+++ model/NewRecipeManager.js
@@ -0,0 +1,15 @@
+class NewRecipe {
+    constructor(title, href, ingredients, thumbnail){
+        this.title = title;
+        this.href = href;
+        this.ingredients = ingredients;
+        this.thumbnail = thumbnail;
+    }
+};
+
+class NewRecipeManager {
+
+    createNewRecipe = () => {
+        
+    }
+}
