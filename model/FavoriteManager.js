ca3d1264f7487e93b0c508a1b0c1883265621033
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-20 13:44:57 +0200

    Update FavoriteManager.js

--- model/FavoriteManager.js
+++ model/FavoriteManager.js
@@ -1,4 +1,17 @@
+class FavoriteRecepie {
+    constructor (title, ingredients, thumbnail){
+        this.title = title;
+        this.ingredients = ingredients;
+        this.thumbnail = thumbnail;
+    }
+}
+
+
 class FavoriteManager {
 
     favorite = []
+
+    addToFavorites = (recipe) => {
+        this.favorite.push(new FavoriteRecepie(recipe.title, recipe.ingredients, recipe.thumbnail));
+    }
 }
